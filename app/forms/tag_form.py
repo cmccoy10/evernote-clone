17f6aa7171c0603bbfcc21518b92ec48d986ea38
Jessica Abels <jessabels92@gmail.com>
2020-11-24 09:00:07 -0800

    remove unique flag on tags and fix validation function

--- app/forms/tag_form.py
+++ app/forms/tag_form.py
@@ -2,13 +2,14 @@ from flask_wtf import FlaskForm
 from wtforms.fields import (
     StringField, IntegerField
 )
+from flask_login import current_user
 from wtforms.validators import DataRequired
 from app.models import Tag
 
 
 def tag_exists(form, field):
     tag_name = field.data
-    tag_name = Tag.query.filter(Tag.name == tag_name).first()
+    tag_name = Tag.query.filter(Tag.name == tag_name and Tag.user_id == current_user.id).first()
     if tag_name:
         raise ValidationError("Tag name already present.")
 
