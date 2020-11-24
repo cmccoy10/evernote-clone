767c4ac3f83e141a343281cf82c84e9eb9fb421f
Wren McPherson <wren.mcpherson@yahoo.com>
2020-11-24 11:02:21 -0500

    tag routes updated

--- app/api/tag_routes.py
+++ app/api/tag_routes.py
@@ -1,43 +1,67 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
 from sqlalchemy.orm import relationship, sessionmaker, joinedload
-from app.models import Tag, User, Note, Notebook
-
+from app.models import db, Tag, User, Note, Notebook
+from app.froms import TagForm
 
 tag_routes = Blueprint('tags', __name__)
+
+
+def validation_errors_to_error_messages(validation_errors):
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f"{field} : {error}")
+    return errorMessages
 
 
 @tag_routes.route('/')
 @login_required
 def load_tags():
     user_id = current_user.get_id()
-    tags = Tag.query.filter(Tag.user_id == user_id)
-    return tags
+    tags = Tag.query.filter_by(Tag.user_id == user_id).all()
+    return { 'tags': [tag.to_dict() for tag in tags] }
 
 
 @tag_routes.route('/', methods=['POST'])
 @login_required
 def make_new_tag():
     user_id = current_user.get_id()
-    form = NewTagForm()
+    form = TagForm()
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         new_tag = Tag(
             name=form.data['name'],
             user_id=user_id
         )
-        # associate to tag
-        return new_tag
-    return 
+        db.session.add(new_tag)
+        db.session.commit()
+        return new_tag.to_dict()
+    return { 'errors': validation_errors_to_error_messages(form.errors) }
 
 
 @tag_routes.route('/<int:tag_id>', methods=['PUT'])
 @login_required
-def edit_tag():
+def edit_tag(tag_id):
     user_id = current_user.get_id()
+    form = TagForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        tag = Tag.query.get(tag_id)
+        tag.name = form.data['name']
+        db.session.commit()
+        return tag.to_dict()
+    return { 'errors': validation_errors_to_error_messages(form.errors) }
 
 
 @tag_routes.route('/<int:tag_id>', methods=['DELETE'])
 @login_required
-def delete_tag():
+def delete_tag(tag_id):
     user_id = current_user.get_id()
+    try:
+        tag = Tag.query.get(tag_id)
+        db.session.delete(tag)
+        db.session.commit()
+        return { 'message': 'Tag successfully deleted.'}
+    except:
+        return { 'error': 'Tag not found.' }
