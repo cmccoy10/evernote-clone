95a9fff660c15a487f87d70f26e18819c9872b98
Cole McCoy <cole.mccoy41@hotmail.com>
2020-11-24 17:05:56 -0600

    updated signup form

---
+++ documentation/state.js
@@ -0,0 +1,39 @@
+{
+    authentication: {
+
+    },
+    user: {
+        id,
+        first_name,
+        last_name,
+        email
+    },
+    notebooks: {
+        notebook_id: {
+            title,
+            owner_id,
+            is_default,
+            notes: ["note_id"]
+        }
+    },
+    notes: {
+        note_id: {
+            title,
+            body,
+            user_id,
+            notebook_id,
+            modified_on,
+            tags: ["tag_id"]
+        }
+    },
+    tags: {
+        tag_id: {
+            name,
+            user_id,
+            notes: ["note_id"]
+        }
+    },
+    currentNote: {
+        note_id || null
+    }
+}
