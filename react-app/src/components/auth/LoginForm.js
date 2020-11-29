b46bcf401c7336d26d96666a6fdb1350936ec6b6
Jessica Abels <jessabels92@gmail.com>
2020-11-29 15:00:38 -0800

    Style login/signup forms

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 import { login } from "../../services/auth";
 //after login to store user in state
-import { connect } from 'react-redux';
-import { loadUser } from '../../store/ducks/user'
-
+import { connect } from "react-redux";
+import { loadUser } from "../../store/ducks/user";
+import { NavLink } from "react-router-dom";
+import "./AuthForm.css";
+import Button from "@material-ui/core/Button";
 
 const LoginForm = ({ authenticated, setAuthenticated, saveUserTostate }) => {
   const [errors, setErrors] = useState([]);
@@ -15,7 +17,7 @@ const LoginForm = ({ authenticated, setAuthenticated, saveUserTostate }) => {
     e.preventDefault();
     const user = await login(email, password);
     if (!user.errors) {
-      saveUserTostate(user)
+      saveUserTostate(user);
       setAuthenticated(true);
     } else {
       setErrors(user.errors);
@@ -35,39 +37,60 @@ const LoginForm = ({ authenticated, setAuthenticated, saveUserTostate }) => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error) => (
-          <div>{error}</div>
-        ))}
+    <div className="container">
+      <h1>Welcome to Clevernote</h1>
+      <div className="form-container">
+        <form onSubmit={onLogin}>
+          <div>
+            {errors.map((error) => (
+              <div>{error}</div>
+            ))}
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              name="email"
+              type="text"
+              placeholder="Email"
+              value={email}
+              onChange={updateEmail}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={updatePassword}
+            />
+            <Button
+              style={{
+                width: "100%",
+                color: "white",
+                backgroundColor: " #00a82d",
+              }}
+              type="submit"
+            >
+              Sign in
+            </Button>
+          </div>
+
+          <div className="link-container">
+            <span>Don't have an account?</span>
+            <NavLink to="/sign-up" exact={true} activeClassName="active">
+              Sign Up
+            </NavLink>
+          </div>
+        </form>
       </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          name="email"
-          type="text"
-          placeholder="Email"
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type="submit">Login</button>
-      </div>
-    </form>
+    </div>
   );
 };
 
-const mapDispatchToProps = dispatch => (
-  {saveUserTostate: (user) => dispatch(loadUser(user))}
-)
+const mapDispatchToProps = (dispatch) => ({
+  saveUserTostate: (user) => dispatch(loadUser(user)),
+});
 
 export default connect(undefined, mapDispatchToProps)(LoginForm);
--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,23 +1,29 @@
 import React, { useState } from "react";
-import { Redirect } from 'react-router-dom';
-import { signUp } from '../../services/auth';
-import { connect } from 'react-redux';
-import { loadUser } from '../../store/ducks/user'
+import { Redirect } from "react-router-dom";
+import { signUp } from "../../services/auth";
+import { connect } from "react-redux";
+import { loadUser } from "../../store/ducks/user";
+import { NavLink } from "react-router-dom";
+import "./AuthForm.css";
+import Button from "@material-ui/core/Button";
 
-const SignUpForm = ({authenticated, setAuthenticated, saveUserTostate}) => {
+const SignUpForm = ({ authenticated, setAuthenticated, saveUserTostate }) => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [repeatPassword, setRepeatPassword] = useState("");
+  const [errors, setErrors] = useState([]);
 
   const onSignUp = async (e) => {
     e.preventDefault();
     if (password === repeatPassword) {
       const user = await signUp(firstName, lastName, email, password);
       if (!user.errors) {
-        saveUserTostate(user)
+        saveUserTostate(user);
         setAuthenticated(true);
+      } else {
+        setErrors(user.errors);
       }
     }
   };
@@ -47,60 +53,85 @@ const SignUpForm = ({authenticated, setAuthenticated, saveUserTostate}) => {
   }
 
   return (
-    <form onSubmit={onSignUp}>
-      <div>
-        <label>First Name</label>
-        <input
-          type="text"
-          name="firstName"
-          onChange={updateFirstName}
-          value={firstName}
-        ></input>
+    <div className="container">
+      <h1>Welcome to Clevernote</h1>
+      <div className="form-container">
+        <form onSubmit={onSignUp}>
+          <div>
+            {errors.map((error) => (
+              <div>{error}</div>
+            ))}
+          </div>
+          <div>
+            <label>First Name</label>
+            <input
+              type="text"
+              name="firstName"
+              onChange={updateFirstName}
+              value={firstName}
+            ></input>
+          </div>
+          <div>
+            <label>Last Name</label>
+            <input
+              type="text"
+              name="lastName"
+              onChange={updateLastName}
+              value={lastName}
+            ></input>
+          </div>
+          <div>
+            <label>Email</label>
+            <input
+              type="text"
+              name="email"
+              onChange={updateEmail}
+              value={email}
+            ></input>
+          </div>
+          <div>
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              onChange={updatePassword}
+              value={password}
+            ></input>
+          </div>
+          <div>
+            <label>Repeat Password</label>
+            <input
+              type="password"
+              name="repeat_password"
+              onChange={updateRepeatPassword}
+              value={repeatPassword}
+              required={true}
+            ></input>
+          </div>
+          <Button
+            style={{
+              width: "100%",
+              color: "white",
+              backgroundColor: " #00a82d",
+            }}
+            type="submit"
+          >
+            Sign Up
+          </Button>
+          <div className="link-container">
+            <span> Already have an account?</span>
+            <NavLink to="/login" exact={true} activeClassName="active">
+              Sign in
+            </NavLink>
+          </div>
+        </form>
       </div>
-      <div>
-        <label>Last Name</label>
-        <input
-          type="text"
-          name="lastName"
-          onChange={updateLastName}
-          value={lastName}
-        ></input>
-      </div>
-      <div>
-        <label>Email</label>
-        <input
-          type="text"
-          name="email"
-          onChange={updateEmail}
-          value={email}
-        ></input>
-      </div>
-      <div>
-        <label>Password</label>
-        <input
-          type="password"
-          name="password"
-          onChange={updatePassword}
-          value={password}
-        ></input>
-      </div>
-      <div>
-        <label>Repeat Password</label>
-        <input
-          type="password"
-          name="repeat_password"
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-          required={true}
-        ></input>
-      </div>
-      <button type="submit">Sign Up</button>
-    </form>
+    </div>
   );
 };
 
-const mapDispatchToProps = dispatch => (
-  {saveUserTostate: (user) => dispatch(loadUser(user))}
-)
+const mapDispatchToProps = (dispatch) => ({
+  saveUserTostate: (user) => dispatch(loadUser(user)),
+});
 
 export default connect(undefined, mapDispatchToProps)(SignUpForm);
