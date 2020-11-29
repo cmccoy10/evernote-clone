b2ea0e1735f568e87c957dedd695362b4c25295c
Jessica Abels <jessabels92@gmail.com>
2020-11-28 18:56:50 -0800

    refactor logic to highlight list item based on current notebook

--- react-app/src/components/Sidebar/AllNotes.js
+++ react-app/src/components/Sidebar/AllNotes.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { ListItem } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStickyNote } from "@fortawesome/free-solid-svg-icons";
+import { setCurrentNotebook } from "../../store/ducks/currentNotebook";
 
 const useStyles = makeStyles((theme) => ({
   icon: {
@@ -18,17 +20,18 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const AllNotes = () => {
-  const [selected, setSelected] = useState(false);
-
-  const handleClick = () => {
-    setSelected(true);
+  const dispatch = useDispatch();
+  const currentNotebook = useSelector((state) => state.currentNotebook);
+  const defaultNotebookId = 5;
+  const handleClick = async () => {
+    await dispatch(setCurrentNotebook(defaultNotebookId));
   };
 
   const classes = useStyles();
   return (
     <div
       className={
-        selected
+        defaultNotebookId === currentNotebook
           ? `allnotes-link-container ${"selected"}`
           : `allnotes-link-container`
       }
