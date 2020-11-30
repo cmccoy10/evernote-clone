ebda7a35ba72a7f24a97febd3976ec2d3d444474
Jessica Abels <jessabels92@gmail.com>
2020-11-29 20:15:12 -0800

    handle logic for loading lists based on selected notebook

--- react-app/src/components/NotesList/Note.js
+++ react-app/src/components/NotesList/Note.js
@@ -2,16 +2,16 @@ import React from "react";
 import "./NotesList.css";
 
 const Note = (props) => {
-  console.log("props", props);
   const tagsList = [];
   props.tags.forEach((tag) => {
     tagsList.push(<li key={tag.id}>{tag.name}</li>);
   });
   const { title, body, updated_on } = props.note;
+
   const formattedTitle = title.replace(/<[^>]+>/g, "");
   return (
     <div className="note-container">
-      <span>{title ? formattedTitle : "Untitled"}</span>
+      <span>{formattedTitle ? formattedTitle : "Untitled"}</span>
       <p>{body}</p>
       <small>{updated_on}</small>
       <ul>{tagsList}</ul>
