36aff3f7b0a95ff17d978f80a256ca2d8477e844
Jessica Abels <jessabels92@gmail.com>
2020-11-30 21:34:08 -0800

    strip html from body

--- react-app/src/components/NotesList/Note.js
+++ react-app/src/components/NotesList/Note.js
@@ -1,33 +1,39 @@
 import React, { useState } from "react";
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import "./NotesList.css";
-import Chip from '@material-ui/core/Chip';
-import { setCurrentNote } from '../../store/ducks/currentNote'
-
+import Chip from "@material-ui/core/Chip";
+import { setCurrentNote } from "../../store/ducks/currentNote";
 
 const Note = (props) => {
-  const dispatch = useDispatch()
-  const currentNote = useSelector(state => state.currentNote)
+  const dispatch = useDispatch();
+  const currentNote = useSelector((state) => state.currentNote);
   const tagsList = [];
   props.tags.forEach((tag) => {
-    tagsList.push(<Chip className='tagItem' key={tag.id} label={tag.name}/>);
+    tagsList.push(<Chip className="tagItem" key={tag.id} label={tag.name} />);
   });
   const { title, body, updated_on } = props.note;
-  const date = new Date(updated_on)
+  const date = new Date(updated_on);
   const options = {
-    day: 'numeric',
-    month: 'short'
-  }
-  const isSelected = currentNote == props.note.id ? true : false
+    day: "numeric",
+    month: "short",
+  };
+  const isSelected = currentNote == props.note.id ? true : false;
   const formattedTitle = title.replace(/<[^>]+>/g, "");
+  const formattedBody = body.replace(/<\/?[^>]+(>|$)/g, "");
+
   return (
-    <div onClick={() => dispatch(setCurrentNote(props.note.id))} className={isSelected ? 'note-div__selected': 'note-div'}>
+    <div
+      onClick={() => dispatch(setCurrentNote(props.note.id))}
+      className={isSelected ? "note-div__selected" : "note-div"}
+    >
       <div className="note-container">
-        <h3><span>{formattedTitle ? formattedTitle : "Untitled"}</span></h3>
-        <p className='note-body-text'>{body}</p>
-        <small>{date.toLocaleDateString('en-En', options)}</small>
+        <h3>
+          <span>{formattedTitle ? formattedTitle : "Untitled"}</span>
+        </h3>
+        <p className="note-body-text">{formattedBody}</p>
+        <small>{date.toLocaleDateString("en-En", options)}</small>
       </div>
-      <div className='tagContainer'>{tagsList}</div>
+      <div className="tagContainer">{tagsList}</div>
     </div>
   );
 };
